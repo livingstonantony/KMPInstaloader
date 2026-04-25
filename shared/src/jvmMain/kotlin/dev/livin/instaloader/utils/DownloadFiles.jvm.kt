fca54322165fe69ab305df4e26006bc240175fae
livingstonantony <livingstonantony70@gmail.com>
2026-04-25 12:32:22 +0530

    Code implementation done for the Desktop (Mac/Linux/Windows)
    actual fun saveImageToFile(bytes: ByteArray, fileName: String): String {
    
    }
    
    actual fun saveVideoToFile(bytes: ByteArray, fileName: String): String {
    
    }
    
    actual fun getCurrentDateTimeString(): String {
    
    }
    
    Note**: Tested only in the Mac M1

--- shared/src/jvmMain/kotlin/dev/livin/instaloader/utils/DownloadFiles.jvm.kt
+++ shared/src/jvmMain/kotlin/dev/livin/instaloader/utils/DownloadFiles.jvm.kt
@@ -1,13 +1,68 @@
 package dev.livin.instaloader.utils
 
-actual fun saveImageToFile(bytes: ByteArray, fileName: String): String {
-    TODO("Not yet implemented")
-}
+// desktopMain
 
-actual fun saveVideoToFile(bytes: ByteArray, fileName: String): String {
-    TODO("Not yet implemented")
-}
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 actual fun getCurrentDateTimeString(): String {
-    TODO("Not yet implemented")
+    val sdf = SimpleDateFormat("yyyy_MM_dd HH_mm_ss", Locale.getDefault())
+    return sdf.format(Date())
+}
+
+/**
+ * Returns:
+ * Pictures/InstaLoader
+ *
+ * Works on macOS / Linux / Windows
+ */
+private fun getPicturesDirectory(): File {
+    val home = System.getProperty("user.home")
+    val picturesDir = File(home, "Pictures/InstaLoader")
+
+    if (!picturesDir.exists()) {
+        picturesDir.mkdirs()
+    }
+
+    return picturesDir
+}
+
+/**
+ * Saves image bytes into:
+ * Pictures/InstaLoader
+ *
+ * Returns absolute file path
+ */
+actual fun saveImageToFile(
+    bytes: ByteArray,
+    fileName: String
+): String {
+    val file = File(
+        getPicturesDirectory(),
+        "$fileName.jpg"
+    )
+
+    file.writeBytes(bytes)
+    return file.absolutePath
+}
+
+/**
+ * Saves video bytes into:
+ * Pictures/InstaLoader
+ *
+ * Returns absolute file path
+ */
+actual fun saveVideoToFile(
+    bytes: ByteArray,
+    fileName: String
+): String {
+    val file = File(
+        getPicturesDirectory(),
+        "$fileName.mp4"
+    )
+
+    file.writeBytes(bytes)
+    return file.absolutePath
 }
